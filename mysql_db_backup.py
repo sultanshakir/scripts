8a21795f76067359520dd480ed9f60619f4e6f84
Kevin Finley <Kevin.Finley@flhosp.org>
2011-06-28 09:35:11 -0400

    added mysql_db_backup.py

---
+++ mysql_db_backup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+MySQL Database Backup...
+"""
+import sys
+import os
+import subprocess
+import string
+
+if (len(sys.argv) > 1):
+  
+  print sys.argv[1]
+
+
+
+else:
+  print "What database should we backup?"
+
+
+
+
