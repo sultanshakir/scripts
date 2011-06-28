81710696088bad3dc4fca1e8a2549c19ef9762f5
Kevin Finley <Kevin.Finley@flhosp.org>
2011-06-28 09:51:08 -0400

    modified mysql_db_backup script...

--- mysql_db_backup.py
+++ mysql_db_backup.py
@@ -10,13 +10,19 @@ import string
 
 if (len(sys.argv) > 1):
   
+  today = datetime.date.today()
+  db_admin_usr = '' 
+  db_admin_pw = ''
+  db_name = sys.argv[1]
+  db_backup_name = sys.argv[1] + str(today) + '.sql' 
+
   print sys.argv[1]
+  p = subprocess.Popen('mysqldump --opt -v --user=' + db_admin_usr + ' --password=' + db_admin_pw + ' ' + db_name + ' > ' + db_backup_name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-
+  for line in p.stdout.readlines():
+    print line,
 
 else:
   print "What database should we backup?"
-
-
 
 
