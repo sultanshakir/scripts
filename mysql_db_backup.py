2d6bf02e7da52ba9397fd4c661086101a0f9467e
Kevin Finley <Kevin.Finley@flhosp.org>
2011-06-28 10:08:29 -0400

    modified backup file name..

--- mysql_db_backup.py
+++ mysql_db_backup.py
@@ -15,13 +15,15 @@ if (len(sys.argv) > 1):
   db_admin_usr = '' 
   db_admin_pw = ''
   db_name = sys.argv[1]
-  db_backup_name = sys.argv[1] + str(today) + '.sql' 
+  db_backup_name = sys.argv[1] + '-' + str(today) + '.sql' 
 
-  print sys.argv[1]
+  print 'Starting backup of db...' + sys.argv[1]
   p = subprocess.Popen('mysqldump --opt -v --user=' + db_admin_usr + ' --password=' + db_admin_pw + ' ' + db_name + ' > ' + db_backup_name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
   for line in p.stdout.readlines():
     print line,
+  
+  print 'Finished backup of db...' + sys.argv[1]
 
 else:
   print "What database should we backup?"
